8a37fc1c003ec83b754888d1ff4eab607fece40d
Gurnoor Deol <gurnoor.deol@forgeahead.io>
2023-04-06 16:46:51 -0700

    Added Store, ToggleCart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
 import classes from './CartButton.module.css';
-
+import { uiActions } from '../../store/ui';
+import { useDispatch } from 'react-redux';
 const CartButton = (props) => {
+
+  const dispatch = useDispatch();
+
+  const showCartHandler = () => {
+    dispatch(uiActions.toggleCart());
+  };
+
   return (
-    <button className={classes.button}>
+    <button onClick={showCartHandler} className={classes.button}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
---
+++ src/store/ui.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+    name: "ui",
+    initialState: {showCart: true},
+    reducers:{
+        toggleCart(state)
+        {
+            state.showCart = !state.showCart;
+        }
+    }
+});
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice.reducer;
